b8f9bdd5f7c22ec638442c406e884feffcd4e5b8
Pedro Henrique de Souza Silvestre <980133@queroquero.local>
2022-10-03 11:21:40 -0300

    atualizando

--- CreateCRM/views.py
+++ CreateCRM/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse            
 
 def index(request):
-    return HttpResponse("CREATE CRM")
+    return render(request, 'base.html')
+
