cc7f4e25b2f65f8b29d51283d940f6009ab2916a
Pedro Henrique de Souza Silvestre <980133@queroquero.local>
2022-10-06 19:52:06 -0300

    atualizando

--- Login/views.py
+++ Login/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('TELA DE LOGIN')
+#Request para login.html mandado para url /login
+def login(request):
+    return render(request, 'login.html')
+
+
+
--- registration/urls.py
+++ registration/urls.py
@@ -1,7 +1,6 @@
-from unicodedata import name
 from django.urls import path  
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.registration_form, name="registration"),
 ]
