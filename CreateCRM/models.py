dd74ab8b0336d5d7f182e01978a45e2fcc21fd63
Pedro Henrique de Souza Silvestre <980133@queroquero.local>
2022-11-03 11:32:20 -0300

    update

--- CreateCRM/models.py
+++ CreateCRM/models.py
@@ -19,7 +19,8 @@ class Create_CRM(models.Model):
         ('Media','media'),
         ('Baixa', 'baixa')
     )
-
+    
+    status_crm = models.CharField(max_length=50, choices=STATUS, default='Pendentes')
     data_criacao = models.DateField()
     descricao = models.TextField()
     objetivo = models.TextField()
@@ -27,8 +28,8 @@ class Create_CRM(models.Model):
     dependencia = models.BooleanField(default=False)
     mostrar_crm = models.BooleanField(default=True)
     empresa = models.CharField(max_length=50)
+    versao = models.IntegerField(default=1)
     status_crm = models.CharField(max_length=50, choices=STATUS, default='Pendentes')
-    versao = models.IntegerField(null=False, default=1)
     complexidade_crm = models.CharField(max_length=50, choices=COMPLEXIDADE, blank=True)
     file = models.FileField(blank=True, upload_to='files/%Y/%m/',)
     solicitante = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
@@ -36,6 +37,4 @@ class Create_CRM(models.Model):
 
     def __str__(self):
         return f'CRM - {self.id}'
-
-
 
